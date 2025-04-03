ac00058260dc9f75b2c94db72e04b642b548ec0a
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-04-03 16:27:46 +0800

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,9 @@
 import './App.css'
-import doge from '@/assets/images/doge.png';
-
-function Link({name, url}) {
-   return (
-       <a href={url} target="_blank" className={"link dela-gothic"}>
-           {name}
-       </a>
-   );
-}
-
-function Links() {
-    return (
-        <ul className={"links"}>
-            <Link name={"Гитхаб"} url={"https://github.com/sqlerrorthing"}/>
-            <Link name={"Телеграм"} url={"https://t.me/sqlerrorph"}/>
-            <Link name={"Инстаграм"} url={"https://instagram.com/sqlerror"}/>
-        </ul>
-    )
-}
-
-function Portfolio() {
-    return (
-        <div className={"portfolio"}>
-            <p>проекты в портфолио пока что пустует...</p>
-            <p>возможно, это не надолго</p>
-        </div>
-    )
-}
 
 function App() {
     return (
         <>
-            <main className={"main"}>
-                <div className={"doge"}>
-                    <img src={doge} alt=""/>
-                </div>
-                <div className="main-container">
-                    <div className="title">
-                        <h3>Привет, я Рома 👋 16 лет. Моё мини-портфолио</h3>
-                        <p>Здесь будут, возможно, мои проекты, но пока что, возможно, оно будет пустовать</p>
-                    </div>
 
-                    <Links/>
-                    <Portfolio/>
-                </div>
-            </main>
         </>
     )
 }
