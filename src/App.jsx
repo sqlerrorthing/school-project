2dcdf9a6e790470e861c1238048575e1bf069843
sqlerrorthing <oneqzx@gmail.com>
2025-04-03 23:04:27 +0800

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import {Navbar} from "@/components/layout/Navbar.jsx";
-import {Navigate, Route, Routes} from "react-router-dom";
-import {Index} from "@/pages/Index.jsx";
-import {Resume} from "@/pages/Resume.jsx";
-import {Autobiography} from "@/pages/Autobiography.jsx";
-import {Diplomas} from "@/pages/Diplomas.jsx";
+import { Navbar } from '@/components/layout/Navbar.jsx'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Index } from '@/pages/Index.jsx'
+import { Resume } from '@/pages/Resume.jsx'
+import { Autobiography } from '@/pages/Autobiography.jsx'
+import { Diplomas } from '@/pages/Diplomas.jsx'
 
 export const App = () => {
-    return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route key="/" path="/" element={<Index />} />
-                <Route key="/resume" path="/resume" element={<Resume />} />
-                <Route key="/autobiography" path="/autobiography" element={<Autobiography />} />
-                <Route key="/diplomas" path="/diplomas" element={<Diplomas />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </>
-    )
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route key="/" path="/" element={<Index />} />
+				<Route key="/resume" path="/resume" element={<Resume />} />
+				<Route
+					key="/autobiography"
+					path="/autobiography"
+					element={<Autobiography />}
+				/>
+				<Route key="/diplomas" path="/diplomas" element={<Diplomas />} />
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</>
+	)
 }
