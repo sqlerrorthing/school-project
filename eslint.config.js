7a9afa3adafa2d7edafd7468a342f1a562350b5b
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-04-03 10:00:37 +0800

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['*'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
