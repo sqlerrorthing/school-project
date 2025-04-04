6cc07d08f7f70762012b172de25086bbc96007b1
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-04-04 13:49:02 +0800

    add prettier & removed autobiography

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Navbar } from '@/components/layout/Navbar.jsx'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Index } from '@/pages/Index.jsx'
 import { Resume } from '@/pages/Resume.jsx'
-import { Autobiography } from '@/pages/Autobiography.jsx'
 import { Diplomas } from '@/pages/Diplomas.jsx'
 
 export const App = () => {
@@ -12,11 +11,6 @@ export const App = () => {
 			<Routes>
 				<Route key="/" path="/" element={<Index />} />
 				<Route key="/resume" path="/resume" element={<Resume />} />
-				<Route
-					key="/autobiography"
-					path="/autobiography"
-					element={<Autobiography />}
-				/>
 				<Route key="/diplomas" path="/diplomas" element={<Diplomas />} />
 				<Route path="*" element={<Navigate to="/" replace />} />
 			</Routes>
