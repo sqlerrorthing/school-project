ea209fc1179afa00996d4bf077b4a6530b6ce06a
sqlerrorthing <sqlerrorthing@gmail.com>
2025-04-07 15:33:58 +0800

    badges for projects

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,14 +1,18 @@
+import { ExternalLinkIcon } from '@/components/ExternalLinkIcon'
 import { useNavigate } from 'react-router-dom'
 
 const Technology = ({ name, description, image, link }) => {
     return (
         <div
-            className="hover:-translate-y-1 hover:translate-x-1 flex cursor-pointer shadow-xl transition-transform transition-shadow hover:shadow-accent/20 duration-300 rounded-xl p-2 gap-2 w-full backdrop-blur"
+            className="group hover:-translate-y-1 hover:translate-x-1 flex cursor-pointer shadow-xl transition-transform transition-shadow hover:shadow-accent/20 duration-300 rounded-xl p-2 gap-2 w-full backdrop-blur"
             onClick={() => window.open(link, '_blank')}
         >
             <img src={`/assets/images/${image}`} className="w-12 h-12" alt="" />
             <div className="flex text-left flex-wrap flex-col">
-                <span className="font-bold nunito text-xl h-12 flex items-center">{name}</span>
+                <div className="flex items-center h-12 gap-2">
+                    <span className="font-bold nunito text-xl">{name}</span>
+                    <ExternalLinkIcon className="opacity-0 w-3 h-3 fill-black group-hover:opacity-100 transition-opacity" />
+                </div>
                 <span>{description}</span>
             </div>
         </div>
@@ -24,15 +28,7 @@ const MiniButton = ({ text, link }) => {
             className="group flex items-center gap-2 cursor-pointer transition-colors hover:text-accent"
         >
             {text}
-            <svg
-                className="w-4 h-4 fill-black group-hover:fill-accent transition-colors translate-y-[2px] opacity-50 group-hover:opacity-100 transition-transform group-hover:scale-125"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 500 500"
-                width="500px"
-                height="500px"
-            >
-                <path d="M 443.3 56.684 L 443.3 354.286 C 443.334 369.555 426.863 379.144 413.604 371.566 C 408.507 368.632 404.983 363.605 403.944 357.846 L 403.636 354.286 L 403.588 129.055 C 403.588 127.578 402.004 126.621 400.69 127.353 C 400.572 127.424 400.421 127.542 400.314 127.66 L 90.565 437.482 C 83.798 444.279 73.133 445.251 65.246 439.798 L 62.536 437.482 C 55.736 430.717 54.767 420.05 60.22 412.163 L 62.536 409.419 L 372.204 99.67 C 373.279 98.629 372.819 96.843 371.387 96.43 C 371.232 96.382 371.044 96.346 370.842 96.346 L 145.695 96.346 C 136.104 96.346 127.91 89.545 126.169 80.11 L 125.863 76.48 C 125.863 66.734 132.863 58.658 142.098 56.954 L 145.729 56.684 L 443.3 56.684 Z" />
-            </svg>
+            <ExternalLinkIcon className="w-4 h-4 fill-black group-hover:fill-accent transition-colors translate-y-[2px] opacity-50 group-hover:opacity-100 transition-transform group-hover:scale-125" />
         </button>
     )
 }
