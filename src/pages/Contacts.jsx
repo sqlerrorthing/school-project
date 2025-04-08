0aba253790dd0d63f72dd9e3ee1a252bd1a1756f
sqlerrorthing <sqlerrorthing@gmail.com>
2025-04-08 22:09:14 +0800

    remove bg

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,8 +1,6 @@
-import clsx from 'clsx'
-
 const ContactMethod = ({ icon, name, username, link = null }) => {
     return (
-        <div className="rounded backdrop-blur-xl w-full h-50 shadow-xl bg-neutral-200/40 flex gap-4 flex-col justify-center items-center">
+        <div className="rounded backdrop-blur-xl w-full h-50 shadow-xl flex gap-4 flex-col justify-center items-center">
             <img className="w-12 h-12" src={`/assets/images/${icon}`} alt="" />
             <div className="flex flex-col justify-center items-center gap-2">
                 <span className="text-xl font-bold">{name}</span>
