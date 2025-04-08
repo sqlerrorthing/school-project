5be7d09c05651debfff007a462483c893cfe2cf7
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-04-08 19:53:06 +0800

    Refactor Resume page to Skills page (#17)

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 const ContactMethod = ({ icon, name, username, link = null }) => {
     return (
         <div className="rounded backdrop-blur-xl w-full h-50 shadow-xl bg-neutral-200/40 flex gap-4 flex-col justify-center items-center">
-            <img className="w-12 h-12" src={`/assets/images/contact/${icon}`} alt="" />
+            <img className="w-12 h-12" src={`/assets/images/${icon}`} alt="" />
             <div className="flex flex-col justify-center items-center gap-2">
                 <span className="text-xl font-bold">{name}</span>
                 {link ? (
@@ -23,38 +23,34 @@ export const Contacts = () => {
         <div className="container mx-auto relative my-16">
             <div className="mx-auto flex flex-col items-center backdrop-blur">
                 <span className="text-6xl font-bold nunito text-center">Связаться со мной</span>
-                <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 w-full mt-5">
-                    <ContactMethod icon="discord.svg" name="Discord" username="@sqlerrorthing" />
-                    <ContactMethod
-                        icon="telegram.svg"
-                        name="Telegram"
-                        username="@sqlerrorph"
-                        link="https://t.me/@sqlerrorph"
-                    />
-                    <ContactMethod
-                        icon="instagram.svg"
-                        name="Instagram"
-                        username="@sqlerrorthing"
-                        link="https://instagram.com/@sqlerrorthing"
-                    />
-                    <ContactMethod
-                        icon="linkedin.svg"
-                        name="LinkedIn"
-                        username="sql errorthing"
-                        link="https://linked.in/@sqlerrorph"
-                    />
-                    <ContactMethod
-                        icon="github.svg"
-                        name="GitHub"
-                        username="@sqlerrorthing"
-                        link="https://github.com/sqlerrorthing"
-                    />
-                    <ContactMethod
-                        icon="email.svg"
-                        name="Email"
-                        username="sqlerrorthing@gmail.com"
-                    />
-                </div>
+            </div>
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 w-full mt-5">
+                <ContactMethod icon="discord.svg" name="Discord" username="@sqlerrorthing" />
+                <ContactMethod
+                    icon="telegram.svg"
+                    name="Telegram"
+                    username="@sqlerrorph"
+                    link="https://t.me/@sqlerrorph"
+                />
+                <ContactMethod
+                    icon="instagram.svg"
+                    name="Instagram"
+                    username="@sqlerrorthing"
+                    link="https://instagram.com/@sqlerrorthing"
+                />
+                <ContactMethod
+                    icon="linkedin.svg"
+                    name="LinkedIn"
+                    username="sql errorthing"
+                    link="https://linked.in/@sqlerrorph"
+                />
+                <ContactMethod
+                    icon="github.svg"
+                    name="GitHub"
+                    username="@sqlerrorthing"
+                    link="https://github.com/sqlerrorthing"
+                />
+                <ContactMethod icon="email.svg" name="Email" username="sqlerrorthing@gmail.com" />
             </div>
         </div>
     )
